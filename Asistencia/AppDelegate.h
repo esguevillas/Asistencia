aa6e622989c444cf484d94d9aae0e3c16ff26601
esguevillas <anagutierrezesguevillas@MacBook-Pro-de-ANA-GUTIERREZ-ESGUEVILLAS.local>
2012-08-02 18:51:49 +0100

    Initial Commit

---
+++ Asistencia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Asistencia
+//
+//  Created by ANA GUTIÉRREZ ESGUEVILLAS GUTIERREZ ESGUEVILLAS on 02/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Asistencia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Asistencia
+//
+//  Created by ANA GUTIÉRREZ ESGUEVILLAS GUTIERREZ ESGUEVILLAS on 02/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
